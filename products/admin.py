1be1163f105fceef44f9a7e6dc32cb61a7493198
RAAFATMAHMOUD0 <raafatmahmoud4600@gmail.com>
2026-01-25 19:28:37 +0200

    add editor summernote

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Category, Brand, Product, ProductImage
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -15,13 +16,14 @@ class ProductImageInline(admin.TabularInline):
     model = ProductImage
     
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(SummernoteModelAdmin):
     list_display = ('name', 'sku', 'price', 'available', 'created', 'updated')
     list_filter = ('available', 'created', 'updated', 'category', 'brand')
     list_editable = ('price', 'available')
     prepopulated_fields = {'slug': ('name',)}
     inlines = [ProductImageInline]
     search_fields = ('name', 'sku', 'description')
+    summernote_fields = '__all__'
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
