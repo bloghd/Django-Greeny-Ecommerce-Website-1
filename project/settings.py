042f44042918fcdb3e75d8b2e90411e3512bd081
RAAFATMAHMOUD0 <raafatmahmoud4600@gmail.com>
2026-01-29 21:50:36 +0200

    add branch api and requirements

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.middleware.locale import LocaleMiddleware
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +124,14 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [BASE_DIR / 'locale']
+USE_I18N = True
+# LANGUAGE_CODE = 'ar'
+# USE_L10N = True
+# TIME_ZONE = 'Africa/Cairo'
+# USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
